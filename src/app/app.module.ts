16d4a472743a4cbf9042a387780efff10a2102ca
Jorge Saez <Jorgesaez952o@gmail.com>
2022-10-24 00:08:33 -0300

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Login/Login.component';
 
 import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './Card/Card.component';
 
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
 
       LoginComponent,
 
-      HeaderComponent
+      HeaderComponent,
+      CardComponent
    ],
   imports: [
     BrowserModule,
