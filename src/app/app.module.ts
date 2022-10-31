2027bd3ce32fdbafec507b96ee217b5e03c667e7
YerkoBarrientosOsorio <72980917+YerkoBarrientosOsorio@users.noreply.github.com>
2022-10-30 21:47:18 -0300

    Login y rutas
    Se agrega login y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './Login/Login.component';
+
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CardComponent } from './Card/Card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      LoginComponent,
+      HeaderComponent,
+      CardComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/rut-validate.service.ts
+++ src/app/services/rut-validate.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UsuarioModel } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,29 +10,9 @@ export class RutValidateService {
 
   constructor(private httpClient: HttpClient) { }
 
-  usuarios = 
-    [
-      {"_id":"6340e72fbec26d93760870e4","rut":"19.990.015-3","pass":"hola1345","__v":0},
-      {"_id":"6340eb4abec26d93760870fb","rut":"13.158.639-5","pass":"doxeadomijo3435","__v":0},
-      {"_id":"6341b80ad669f2639d933127","rut":"67.797.765-6","pass":"pruebaandeoid12","__v":0},
-      {"_id":"63450eb7fdb468b41b8fefd4","rut":"20.443.221.3","pass":"probandoaaa","__v":0},
-      {"_id":"6345be9e862d07c2c9a8ba6f","rut":"9.331.442.K","pass":"contraseñaa","__v":0}
-    ]
-  
 
-  public getUsuarios(rut?: string, pass?: string): boolean{
-
-    let isOK: boolean = false;
-
-    this.usuarios.forEach(user => {
-      if(rut==user.rut&&pass==user.pass){
-        isOK = true;
-      }
-    });
-
-    return isOK;
-    
-
+  public getAuth(): Observable<UsuarioModel[]>{
+    return this.httpClient.get<UsuarioModel[]>('http://portal-docente.herokuapp.com/api/usuarios');
   }
 
 
