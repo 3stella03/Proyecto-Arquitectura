d4bb3ed03ef1682dfb090b504e5c450520ced45e
YerkoBarrientosOsorio <72980917+YerkoBarrientosOsorio@users.noreply.github.com>
2022-11-24 23:08:40 -0300

    Iterate subjects

---
+++ src/app/services/get-subjects.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { SubjectModel } from '../models/subject.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetSubjectsService {
+
+constructor(private httpClient: HttpClient) { }
+
+public getSubjects(): Observable<SubjectModel[]>{
+  return this.httpClient.get<SubjectModel[]>('https://portaldocentetwk2022.herokuapp.com/api/ramos');
+}
+
+
+
+
+}
--- src/app/side_bar/side_bar.component.ts
+++ src/app/side_bar/side_bar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Input } from '@angular/core';
+import { GetSubjectsService } from '../services/get-subjects.service';
+import { SubjectModel } from '../models/subject.model';
 @Component({
   selector: 'app-side_bar',
   templateUrl: './side_bar.component.html',
   styleUrls: ['./side_bar.component.css']
 })
 export class Side_barComponent implements OnInit {
+
+  subjects: SubjectModel[]=[];
   
-  constructor(private router: Router) {
+  constructor(private getSubjectsService: GetSubjectsService, private router: Router) {
+
+    this.getSubjects();
+
    }
+
+   getSubjects = () => {
+    this.getSubjectsService.getSubjects().subscribe((data: SubjectModel[])=>{
+      this.subjects = data;
+      console.log(this.subjects);
+    });
+  }
 
   ngOnInit() {
   }
